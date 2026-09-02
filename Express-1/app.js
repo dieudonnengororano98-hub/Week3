cbe087445342b0622c8bc6ac8f0e85c656577496
dieudonneshaka <dieudonnengororano98@gmail.com>
2026-09-02 19:28:23 +0300

    add cats API endpoint

---
+++ Express-1/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+const hostname = "127.0.0.1";
+const app = express();
+const port = 3010;
+
+app.get("/", (req, res) =>{
+    res.send("Hello world");
+});
+
+
+app.get('/api/v1/cats', (req, res) => {
+  const cat = {
+    cat_id: 1,
+    name: 'Milo',
+    birthdate: '2022-05-10',
+    weight: 4.5,
+    owner: 'John',
+    image: 'https://loremflickr.com/320/240/cat'
+  };
+
+  res.json(cat);
+});
+
+
+app.listen(port, hostname, () =>{
+    console.log(`server running at http://${hostname}:${port}/`);
+})
