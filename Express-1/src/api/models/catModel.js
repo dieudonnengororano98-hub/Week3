ff29770f49d05f3eea054946be82a22ff1010915
dieudonneshaka <dieudonnengororano98@gmail.com>
2026-09-04 00:58:14 +0300

    controllers, models,routes,app.js and index.js updated

--- Express-1/src/api/models/catModel.js
+++ Express-1/src/api/models/catModel.js
@@ -1,13 +1,11 @@
 const catItems = [
-  { cat_id: 1, name: 'Whiskers', weight: 4.5 },
-  { cat_id: 2, name: 'Felix', weight: 5.2 }
+  {cat_id: 1, name: "whiskers", weight: 4.5 },
+  {cat_id: 2, name: "Felix", weight: 5.2 }
+
 ];
-
 const getCatsFromModel = () => catItems;
-
-const getCatByIdFromModel = (id) => catItems.find((cat) => cat.cat_id === id);
-
-const addCatToModel = (catData) => {
+const getCatByIdFromModel = (id) => catItems.find((cat) => cat.cat_id === id );
+const addCatToModel = (catData) =>{
   const newCat = {
     cat_id: catItems.length ? catItems[catItems.length - 1].cat_id + 1 : 1,
     ...catData
@@ -15,5 +13,5 @@ const addCatToModel = (catData) => {
   catItems.push(newCat);
   return newCat;
 };
+export {getCatsFromModel, getCatByIdFromModel, addCatToModel };
 
-export { getCatsFromModel, getCatByIdFromModel, addCatToModel };
--- Express-1/src/app.js
+++ Express-1/src/app.js
@@ -3,25 +3,24 @@ const hostname = "127.0.0.1";
 const app = express();
 const port = 3000;
 
-app.use('/public', express.static('public'));
+app.use("/public", express.static("public"));
 
 
 app.get("/", (req, res) =>{
-    res.send("Hello world");
+  res.send("Hello world");
 });
 
-
-app.get('/api/v1/cats', (req, res) => {
+app.get("/api/v1/cats", (req, res) => {
   const cat = {
     cat_id: 1,
-    name: 'Milo',
-    birthdate: '2022-05-10',
+    name: "milo",
+    birthdate: "2022-05-10",
     weight: 4.5,
-    owner: 'John',
-    image: 'https://loremflickr.com/320/240/cat'
-  };
+    owner: "john",
+    image: "https://loremflickr.com/320/240/cat"
 
-  res.json(cat);
+  };
+  res.json(cat)
 });
 
 
